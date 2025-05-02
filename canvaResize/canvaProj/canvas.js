3f855d151d1e967e91d5d287ebe51bcb775f9843
richardspink2 <wowbasher45@gmail.com>
2025-05-01 20:11:36 -0600

    Add files via upload

---
+++ canvaResize/canvaProj/canvas.js
@@ -0,0 +1,93 @@
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+var c = canvas.getContext('2d');
+// c.fillStyle = 'rgba(5,230,100,.5)';
+// c.fillRect(100,100,100,100);
+// c.fillStyle = 'rgba(255,10,100,.5)';
+// c.fillRect(400,100,100,100);
+// c.fillStyle = 'rgba(255,50,100,.5)';
+// c.fillRect(300,300,100,100);
+// console.log(canvas);
+
+// //line
+// c.beginPath();
+// c.moveTo(50,300);
+// c.lineTo(300,100);
+// c.lineTo(400,100);
+
+// c.strokeStyle = "";
+// c.stroke();
+// //arc
+
+
+// for (var i = 0; i < 1000 ; ++i)
+//     {
+    
+//     var x = Math.random() * window.innerWidth;
+//     var y = Math.random() * window.innerHeight;
+//     c.beginPath();
+//     c.arc(x, y, 30, 0, Math.PI * 2, false);
+//     c.strokeStyle = "pink";
+//     c.stroke();    
+// }
+
+
+function Circle(x, y, dx,dy,radius){
+    this.dx = dx;
+    this.dy = dy;
+    this.x = x;
+    this.y = y;
+    this.radius = radius
+    this.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = 'green';
+    c.stroke();
+    c.fill();
+    
+}
+this.update = function(){
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0 ) {
+        this.dx = -this.dx;
+    }
+    if(this.y + this.radius > innerHeight || this.y - this.radius < 0){
+        this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+
+}
+}
+
+var circleArray = [];
+
+for(var i = 0; i < 100; ++i){
+    var radius = 30;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var dy = (Math.random() - .5) * 12;
+    var dx = (Math.random() - 0.5) * 12;
+    
+    circleArray.push(new Circle(x,y,dx,dy,radius));
+}
+
+
+
+
+
+
+function animate(){
+    requestAnimationFrame(animate);
+    c.clearRect(0,0, innerWidth, innerHeight);
+
+    for(var i = 0; i < circleArray.length; i++){
+        circleArray[i].update();
+
+    }
+    
+
+}
+animate();
